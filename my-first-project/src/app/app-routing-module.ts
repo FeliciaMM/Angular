985a146ab7801c211eef5337711b2b5f85e0552e
Felicia Militaru <felicia.militaru@totalsoft.ro>
2025-10-23 14:52:26 +0300

    routing merge

--- my-first-project/src/app/app-routing-module.ts
+++ my-first-project/src/app/app-routing-module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Welcome } from './pages/welcome/welcome';
+import { Conferences } from './pages/conferences/conferences';
+import { MyConferences } from './pages/my-conferences/my-conferences';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'pages/welcome', component: Welcome },
+  { path: 'pages/conferences', component: Conferences },
+  { path: 'pages/my-conferences', component: MyConferences },
+  { path: '**', redirectTo: 'welcome' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
