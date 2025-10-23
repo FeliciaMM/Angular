2095a60edefa81f5aa9f56e8e5f7c415de6196d3
Gabriela Ieremie <gabriela.ieremie@totalsoft.ro>
2025-10-23 17:04:56 +0300

    puternic dezvoltat

--- my-first-project/src/app/pages/conferences/conferences.ts
+++ my-first-project/src/app/pages/conferences/conferences.ts
@@ -1,3 +1,35 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ConferenceService, Conference } from '../../services/conferences.service';
+
+// @Component({
+//   standalone: false,
+//   selector: 'app-conferences',
+//   templateUrl: './conferences.html',
+//   styleUrls: ['./conferences.scss'],
+// })
+// export class ConferencesComponent implements OnInit {
+//   conferences: Conference[] = [];
+//   isLoading = true;
+//   errorMessage = '';
+
+//   constructor(private conferenceService: ConferenceService) {}
+
+//   ngOnInit(): void {
+//     this.conferenceService.getConferences().subscribe({
+//       next: (data) => {
+//         this.conferences = data.conferences;
+//         this.isLoading = false;
+//       },
+//       error: (error) => {
+//         this.errorMessage = 'Error loading conferences';
+//         console.error(error);
+//         this.isLoading = false;
+//       },
+//     });
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { ConferenceService, Conference } from '../../services/conferences.service';
 
@@ -15,14 +47,21 @@ export class ConferencesComponent implements OnInit {
   constructor(private conferenceService: ConferenceService) {}
 
   ngOnInit(): void {
+    this.loadConferences();
+  }
+
+  loadConferences(): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+    
     this.conferenceService.getConferences().subscribe({
       next: (data) => {
         this.conferences = data.conferences;
         this.isLoading = false;
       },
       error: (error) => {
-        this.errorMessage = 'Error loading conferences';
-        console.error(error);
+        this.errorMessage = 'Error loading conferences. Please try again.';
+        console.error('Error loading conferences:', error);
         this.isLoading = false;
       },
     });
