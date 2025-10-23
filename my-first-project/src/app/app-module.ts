cf9d5383ed2d26b15142e1b84d978a7f7cc35973
Felicia Militaru <felicia.militaru@totalsoft.ro>
2025-10-23 14:40:55 +0300

    primul commit

--- my-first-project/src/app/app-module.ts
+++ my-first-project/src/app/app-module.ts
@@ -1,30 +1,19 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { Sidebar } from './layout/sidebar/sidebar';
-import { ContentBar } from './layout/content-bar/content-bar';
-import { Welcome } from './pages/welcome/welcome';
-import { Conferences } from './pages/conferences/conferences';
-import { MyConferences } from './pages/my-conferences/my-conferences';
 
 @NgModule({
   declarations: [
-    App,
-    Sidebar,
-    ContentBar,
-    Welcome,
-    Conferences,
-    MyConferences
+    App
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideClientHydration(withEventReplay())
+    provideBrowserGlobalErrorListeners()
   ],
   bootstrap: [App]
 })
