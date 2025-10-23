a43482ad5f5d8d0fdbf657777890b516ad643e73
Eugen Marica <eugen.marica@totalsoft.ro>
2025-10-23 14:46:18 +0300

    pages

--- my-first-project/src/app/app-module.ts
+++ my-first-project/src/app/app-module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Welcome } from './pages/welcome/welcome';
+import { Conferences } from './pages/conferences/conferences';
+import { MyConferences } from './pages/my-conferences/my-conferences';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Welcome,
+    Conferences,
+    MyConferences
   ],
   imports: [
     BrowserModule,
